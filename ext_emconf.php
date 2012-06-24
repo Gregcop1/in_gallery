3795400b416fd2536897b46953ca47d3d7e6843d
Grégory Copin <gcopin@inouit.com>
2012-06-24 11:54:42 +0200

    Initialisation de l'extension

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "gc_lib".
+#
+# Auto generated 14-06-2012 21:09
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Gallery',
+	'description' => '',
+	'category' => '',
+	'author' => 'Grégory Copin, Jérémy Viste',
+	'author_email' => 'gcopin@inouit.com, jviste@inouit.com',
+	'shy' => '',
+	'dependencies' => 'gc_lib',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'Inouit',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+	'suggests' => array(
+	),
+);
+
+?>
