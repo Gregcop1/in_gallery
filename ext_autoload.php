95d540837c8f7c360056b85d8ffc9c54404dd2e9
Grégory Copin <gcopin@inouit.com>
2012-06-24 11:54:42 +0200

    Initialisation de l'extension

---
+++ ext_autoload.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Register necessary class names with autoloader
+ *
+ * $Id: ext_autoload.php $
+ */
+
+ 
+ $extensionPath = t3lib_extMgm::extPath('in_gallery');
+return array(
+	/*'tx_gcblog_categoryList' => $extensionPath . 'class.tx_gcblog_categoryList.php',*/
+);
+unset($extensionPath); 
+?>
